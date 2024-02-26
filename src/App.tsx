e2c3eebf771cc53dbafdca61752658f1b3d55eac
AlexandreCSilva <alecosta.silva12@gmail.com>
2024-02-26 12:00:37 -0300

    feat: basic setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './reset.css';
+import PageRouter from './routes';
+
+function App() {
+  return (
+    <div className="App">
+      <PageRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            teste
+        </div>
+    );
+}
+
+export default HomePage;
